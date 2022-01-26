e939995465095cd8dd4be132fab05e328f4ca495
sb <sarah.beth@unabridgedsoftware.com>
2022-01-26 10:00:18 -0500

    add photos as route and has_many

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'locations#index'
 
   resources :locations
+  resources :photos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
